957241479b0145f75de693e267212b0d5023c5a6
Fortino Sandoval <fortysc97@gmail.com>
2020-12-23 20:07:36 -0800

    ui subscription

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 import { AuthService } from 'src/app/services/auth.service';
+
+import * as ui from 'src/app/shared/ui.actions';
 
 import Swal from 'sweetalert2'
 
@@ -11,22 +16,31 @@ import Swal from 'sweetalert2'
   styles: [
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   isLoading: boolean = false;
+  uiSubscription: Subscription;
 
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { }
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => {
+      this.isLoading = ui.isLoading;
+    });
+  }
+
+  ngOnDestroy() {
+    this.uiSubscription.unsubscribe();
   }
 
   login() {
     if (this.loginForm.invalid || this.isLoading) return;
-    this.isLoading = true;
+    this.store.dispatch(ui.isLoading());
 
     const { email, password } = this.loginForm.value;
     this.auth.login(email, password)
@@ -40,7 +54,7 @@ export class LoginComponent implements OnInit {
           text: message
         })
       })
-      .finally(() => this.isLoading = false);
+      .finally(() => this.store.dispatch(ui.stopLoading()));
   }
 
 }
