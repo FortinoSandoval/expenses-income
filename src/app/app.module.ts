362ebb09aeb843c3f3e81fd7610e1de9794209f4
Fortino Sandoval <fortysc97@gmail.com>
2020-12-23 16:57:21 -0800

    base assets and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExpensesIncomeComponent } from './expenses-income/expenses-income.component';
+import { StatsComponent } from './expenses-income/stats/stats.component';
+import { DetailComponent } from './expenses-income/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ExpensesIncomeComponent,
+    StatsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/expenses-income/expenses-income.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expenses-income',
+  templateUrl: './expenses-income.component.html',
+  styles: [
+  ]
+})
+export class ExpensesIncomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
