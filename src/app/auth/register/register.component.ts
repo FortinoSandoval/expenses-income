d2388388ae1302470ad6ac9143ab9945755907e7
Fortino Sandoval <fortysc97@gmail.com>
2020-12-23 20:21:57 -0800

    using ui isLoading in login/register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+// NgRx
+import { Store } from '@ngrx/store';
+import * as ui from 'src/app/shared/ui.actions';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+
+// Services
 import { AuthService } from 'src/app/services/auth.service';
 
 import Swal from 'sweetalert2';
@@ -12,11 +19,12 @@ import Swal from 'sweetalert2';
   styles: [
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   registerForm: FormGroup;
   isLoading: boolean = false;
+  uiSubscription: Subscription;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -24,11 +32,20 @@ export class RegisterComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => {
+      this.isLoading = ui.isLoading;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
   }
 
   createUser() {
     if (this.registerForm.invalid || this.isLoading) return;
     this.isLoading = true;
+    this.store.dispatch(ui.isLoading());
 
     const { name, email, password } = this.registerForm.value;
     this.authService.createUser(name, email, password)
@@ -42,6 +59,6 @@ export class RegisterComponent implements OnInit {
           text: message
         })
       })
-      .finally(() => this.isLoading = false);
+      .finally(() => this.store.dispatch(ui.stopLoading()));
   }
 }
