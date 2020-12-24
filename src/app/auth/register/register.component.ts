ffb53e62afd86d40a0019e3f0d8a8a51688735ea
Fortino Sandoval <fortysc97@gmail.com>
2020-12-23 18:10:08 -0800

    firebase config

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
+  createUser() {
+    if (this.registerForm.invalid) return;
+    const { name, email, password } = this.registerForm.value;
+    this.authService.createUser(name, email, password).then(res => {
+      console.log(res);
+    }).catch(console.error);
+  }
 }
