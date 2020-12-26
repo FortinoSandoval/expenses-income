1e674b86ff62a9aa5df01998d765c693bd48fe5f
Fortino Sandoval <fortysc97@gmail.com>
2020-12-25 19:09:10 -0800

    loading after add expense/income

--- src/app/expenses-income/expenses-income.component.ts
+++ src/app/expenses-income/expenses-income.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import Swal from 'sweetalert2';
+import { AppState } from '../app.reducer';
 import { ExpensesIncome } from '../models/expenses-income.model';
 import { ExpenseIncomeService } from '../services/expense-income.service';
+import * as ui from '../shared/ui.actions';
 
 @Component({
   selector: 'app-expenses-income',
@@ -10,30 +14,42 @@ import { ExpenseIncomeService } from '../services/expense-income.service';
   styles: [
   ]
 })
-export class ExpensesIncomeComponent implements OnInit {
+export class ExpensesIncomeComponent implements OnInit, OnDestroy {
   incomeForm: FormGroup;
   type: string = 'income';
+  isLoading: boolean = false;
+  uiSubscription: Subscription;
 
-  constructor(private fb: FormBuilder, private incomeExpenseService: ExpenseIncomeService) { }
+  constructor(private fb: FormBuilder, private incomeExpenseService: ExpenseIncomeService, private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.incomeForm = this.fb.group({
       description: ['', Validators.required],
       amount: ['', Validators.required]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => this.isLoading = ui.isLoading);
   }
 
-  save() {
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
+  }
+
+  save(): void {
     if (this.incomeForm.invalid) return;
+    this.store.dispatch(ui.isLoading());
 
     const { description, amount } = this.incomeForm.value;
 
     const expenseIncome = new ExpensesIncome(description, amount, this.type);
 
-    this.incomeExpenseService.createExpenseIncome(expenseIncome).then(() => {
-      this.incomeForm.reset();
-      Swal.fire('Registry created', description, 'success');
-    }).catch(({ message }) => Swal.fire('Registry created', message, 'success'));
-
+    this.incomeExpenseService.createExpenseIncome(expenseIncome)
+      .then(() => {
+        this.incomeForm.reset();
+        Swal.fire('Registry created', description, 'success');
+        this.store.dispatch(ui.stopLoading());
+      })
+      .catch(({ message }) => Swal.fire('Registry created', message, 'success'))
+      .finally(() => this.store.dispatch(ui.stopLoading()));
   }
 }
